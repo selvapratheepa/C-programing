3a2a9e7f725c9a9a09f17773c3e6e832d400f104
selvapratheepa <selvapratheepa12@gmail.com>
2025-08-29 11:59:06 +0530

    Add files via upload

---
+++ selvapratheepa C CSE D/InputOutput/Q4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("enter a fractional num:");
+	scanf("%f",&a);
+	printf("%f",a);
+	return 0;
+}
---
+++ selvapratheepa C CSE D/InputOutput/Q6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("enter a fractional num:");
+	scanf("%f",&a);
+	printf("%.2f",a);
+	return 0;
+}
